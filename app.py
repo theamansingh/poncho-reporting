276b02f637fb9b30c411c8521e987296f0a90c3c
theamansingh <amansingh4440@gmail.com>
2021-01-18 16:16:42 +0530

    Final build

--- app.py
+++ app.py
@@ -6,23 +6,16 @@ import requests
 TEMPLATE_DIR = os.path.abspath('templates')
 STATIC_DIR = os.path.abspath('static')
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
-# user_access = False
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
 @app.route('/index')
 def index():
     return render_template('index.html')
-
-
-# @app.route('/validation', methods=['POST', 'GET'])
-# def login_validation():
-#     email = request.form.get('email')
-#     password = request.form.get('password')
-
-#     if email == 'admin@box8.in' and password == 'box8@123':
-#         return render_template('reporting.html')
-#     else:
-#         return render_template('index.html')
 
 
 @app.route('/reporting')
@@ -35,7 +28,13 @@ def reporting():
         'https://be.redash.box8.co.in/api/queries/14996/results.json?api_key=em5Y2v5Tm033s9hj97QI6U6yXrWDYZL8R340uI9c')
     crm_data_1 = json.loads(crm_data_1.text)[
         'query_result']['data']['rows']
-    return render_template('reporting.html', crm_data=crm_data, crm_data_1=crm_data_1)
+
+    crm_data_2 = requests.get(
+        'https://be.redash.box8.co.in/api/queries/15074/results.json?api_key=REH2iM8Dx1oEHqy4WWCrlVB25pEYIbGAWLVAOsqF')
+    crm_data_2 = json.loads(crm_data_2.text)[
+        'query_result']['data']['rows']
+
+    return render_template('reporting.html', crm_data=crm_data, crm_data_1=crm_data_1, crm_data_2=crm_data_2)
 
 
 if __name__ == "__main__":

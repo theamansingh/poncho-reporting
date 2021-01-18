e89f30d0ec39d93e2d121693880e7a7bc9f73442
Aman <amansingh4440@gmail.com>
2021-01-18 16:30:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ STATIC_DIR = os.path.abspath('static')
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
 
 @app.route('/index')
